bfc6eecad988fd4955aa51ab4b35dfbc76378109
Jonathan Hosea <jonathan.hosea@hotmail.com>
2017-10-16 11:40:24 +0700

    oops ada lupa di stage

--- resources/views/payment/transfer.blade.php
+++ resources/views/payment/transfer.blade.php
@@ -3,6 +3,23 @@
 @section('title', 'Transfer Payment')
 
 @section('content')
-
-
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card text-center">
+                    <div class="card-header">
+                        Transfer Payment
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title">Please Pay To</h4>
+                        <p class="card-text">LALALALALA a/n lalalalala</p>
+                        <a href="{{ URL::to('/verify/') }}" class="btn btn-primary">Verify Payment</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        7 Days
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
